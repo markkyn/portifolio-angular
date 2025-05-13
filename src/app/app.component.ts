785aec4e55480457259b070806a9bfdf444bb89a
Marcos Gabriel <marcos.gabriel@ossirian.com>
2025-05-13 13:55:50 -0300

    new: fixed nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent],
+  imports: [
+    RouterOutlet, RouterLink, RouterLinkActive, 
+    FooterComponent, NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
